23f72795dd1a7478a60d4f9be7980ddc52dbfec8
Immanuel Phiong <immanuel.phi09@gmail.com>
2024-02-03 00:10:56 +1100

    Finished the Code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,85 +5,48 @@ import org.apache.commons.csv.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
-import java.util.Stack;
 
 public class Main {
-
-static class Person {
-    private int id;
-    private String nama;
-    private String alamat;
-
-    public Person(int id, String nama, String alamat) {
-     this.id = id;
-     this.nama = nama;
-     this.alamat = alamat;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNama() {
-        return nama;
-    }
-
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    public String getAlamat() {
-        return alamat;
-    }
-
-    public void setAlamat(String alamat) {
-        this.alamat = alamat;
-    }
-    public String toString(){
-        String[] temp = {String.valueOf(id), nama, alamat};
-        return String.join(";",temp);
-    }
-}
-
-    private static Person createPerson(String[] metadata) {
-        int id = Integer.parseInt(metadata[0]);
-        String nama = metadata[1];
-        String alamat = metadata[2];
-
-        // create and return book of this metadata
-        return new Person(id, nama, alamat);
-    }
-
 public static void main(String[] args) throws IOException {
     //read and store
     String filename = "Book1.csv";
     String[] headers = {"ID", "Nama", "Alamat"};
-    ArrayList<Person> persons = new ArrayList<>();
+    ArrayList<String[]> bank = new ArrayList<>();
     Reader in = new FileReader(filename);
     CSVFormat csvFormat = CSVFormat.EXCEL.builder().setDelimiter(';').setHeader(headers).setSkipHeaderRecord(true).build();
     Iterable<CSVRecord> records = csvFormat.parse(in);
 
     for (CSVRecord record : records) {
         String[] temp = {record.get("ID"), record.get("Nama"), record.get("Alamat")};
-        Person person = createPerson(temp);
-        persons.add(person);
+        bank.add(temp);
     }
 
-    //set new data
+    //accept and set new data
     Scanner scanner = new Scanner(System.in);
-
+    int size = bank.size();
+    int id = Integer.parseInt(bank.get(size-1)[0]) + 1;
+    boolean cond = true;
+    while (cond){
+        System.out.print("Nama : ");
+        String name = scanner.nextLine();
+        if (name.contentEquals("")) {
+            break;
+        }
+        System.out.print("Alamat : ");
+        String address = scanner.nextLine();
+        String[] data = {String.valueOf(id), name, address};
+        bank.add(data);
+        id ++;
+        System.out.println();
+    }
 
     //write back into the file
-    CSVFormat printFormat = CSVFormat.EXCEL.builder().setDelimiter(',').setHeader("ID;Nama;Alamat").build();
+    CSVFormat printFormat = CSVFormat.EXCEL.builder().setDelimiter(';').setHeader(headers).build();
     try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
          CSVPrinter printer = new CSVPrinter(writer, printFormat)) {
-        for (Person i : persons){
+        for (String[] i : bank){
             try {
-                printer.printRecord(i);
+                printer.printRecords((Object) i);
             } catch (IOException e) {
                 e.printStackTrace();
             }
